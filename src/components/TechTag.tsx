9c40836e2d284f5298891d7e651ba4913a04901e
StfnVlds <stefanie.walstab@gmail.com>
2025-05-12 09:07:41 +0200

    add TechTag-component

---
+++ src/components/TechTag.tsx
@@ -0,0 +1,20 @@
+import styles from "./TechTag.module.css";
+
+function TechTag({ tech }: { tech: string }) {
+  let src: string = "./src/assets/img/logo/";
+  switch (tech) {
+    case "javascript":
+      src += "javascript-logo.png";
+      break;
+    case "typescript":
+      src += "typescript-logo.svg";
+      break;
+    case "react":
+      src += "react-icon.svg";
+      break;
+  }
+
+  return <img src={src} className={styles.tag} />;
+}
+
+export default TechTag;
