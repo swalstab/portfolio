ad34dbc3503ea114833fee93b15ec1ddc60b795f
StfnVlds <stefanie.walstab@gmail.com>
2025-05-13 16:55:14 +0200

    add dark-light-mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import { lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import useLocalStorage from "use-local-storage";
+
+import "./App.css";
 
 const PageNav = lazy(() => import("./components/PageNav"));
 const HomePage = lazy(() => import("./pages/HomePage"));
@@ -8,18 +11,29 @@ const ProjectsPage = lazy(() => import("./pages/ProjectsPage"));
 const Footer = lazy(() => import("./components/Footer"));
 
 function App() {
+  const defaultDark: boolean = window.matchMedia(
+    "(prefers-color-scheme: dark"
+  ).matches;
+  const [isDark, setIsDark] = useLocalStorage("isDark", defaultDark);
+
+  function handleThemeButton(): void {
+    setIsDark(() => !isDark);
+  }
+
   return (
-    <BrowserRouter>
-      <PageNav />
+    <div className="app" data-theme={isDark ? "dark" : "light"}>
+      <BrowserRouter>
+        <PageNav isDark={isDark} handleThemeButton={handleThemeButton} />
 
-      <Routes>
-        <Route index element={<HomePage />} />
-        <Route path="about" element={<AboutPage />} />
-        <Route path="projects" element={<ProjectsPage />} />
-      </Routes>
+        <Routes>
+          <Route index element={<HomePage />} />
+          <Route path="about" element={<AboutPage />} />
+          <Route path="projects" element={<ProjectsPage />} />
+        </Routes>
 
-      <Footer />
-    </BrowserRouter>
+        <Footer />
+      </BrowserRouter>
+    </div>
   );
 }
 
