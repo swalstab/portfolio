dced7fb1fe7f2e5a038d35e3d91dc22c272205bc
StfnVlds <stefanie.walstab@gmail.com>
2025-05-13 11:23:48 +0200

    update ProjectsPage

--- src/pages/ProjectsPage.tsx
+++ src/pages/ProjectsPage.tsx
@@ -1,13 +1,43 @@
-// import PageNav from "./PageNav";
+import { lazy } from "react";
 import styles from "./ProjectsPage.module.css";
+
+const ProjectCard = lazy(() => import("../components/ProjectCard"));
+
+interface HasLink {
+  exist: boolean;
+  source: string | null;
+}
+
+export interface ProjectProps {
+  id: string;
+  title: string;
+  description: string;
+  image: string;
+  technologies: string[];
+  sourceCode: HasLink;
+  website: HasLink;
+}
+
+async function getData(endpoint: string): Promise<ProjectProps[]> {
+  const res = await fetch(endpoint);
+
+  return res.json();
+}
+
+const projects = await getData("../../projects.json");
 
 function ProjectsPage() {
   return (
     <main className={styles.projects}>
-      {/* <PageNav /> */}
-
       <section>
-        <h1>Projects</h1>
+        <div className="container">
+          <h2 className="heading-secondary">Projects</h2>
+        </div>
+        <div className="container grid grid--3-cols">
+          {projects.map((p: ProjectProps) => (
+            <ProjectCard key={p.id} project={p} />
+          ))}
+        </div>
       </section>
     </main>
   );
