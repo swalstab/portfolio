b96606a5b40a70a5ebdc37af4888022b0cdb9279
StfnVlds <stefanie.walstab@gmail.com>
2025-05-13 09:50:45 +0200

    add tech-icons

--- src/components/TechTag.tsx
+++ src/components/TechTag.tsx
@@ -4,7 +4,7 @@ function TechTag({ tech }: { tech: string }) {
   let src: string = "./src/assets/img/logo/";
   switch (tech) {
     case "javascript":
-      src += "javascript-logo.png";
+      src += "javascript-logo.svg";
       break;
     case "typescript":
       src += "typescript-logo.svg";
