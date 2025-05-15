6c4c91ae291bd6b878afc79f647c5866ce39ad66
StfnVlds <stefanie.walstab@gmail.com>
2025-05-15 17:26:01 +0200

    update PageNav

--- src/components/PageNav.tsx
+++ src/components/PageNav.tsx
@@ -1,28 +1,57 @@
-import { NavLink, Link } from "react-router-dom";
-import Logo from "./Logo";
-import styles from "./PageNav.module.css";
+import { NavLink, Link, useLocation } from "react-router-dom";
 
-function PageNav() {
+import "./PageNav.css";
+
+function PageNav({
+  isDark,
+  handleThemeButton,
+}: {
+  isDark: boolean;
+  handleThemeButton: () => void;
+}) {
+  const isHome: boolean = useLocation().pathname === "/";
+  const imgGit: string =
+    isDark || isHome ? "github-mark-white.svg" : "github-mark.svg";
+  const imgTheme: string = isDark ? "sun.svg" : "moon.svg";
+
   return (
-    <header className={styles.header}>
-      <Logo />
+    <header className={`header ${isHome ? "dark" : ""}`}>
+      <Link to="/">
+        <img
+          src="./src/assets/img/logo/logo-sw.svg"
+          alt="SW logo"
+          className="main-logo"
+        />
+      </Link>
 
       <nav>
-        <ul>
+        <ul className="nav">
+          <li>
+            <NavLink className="nav-item" to="/about">
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="nav-item" to="/projects">
+              Projects
+            </NavLink>
+          </li>
           <li>
             <Link to="https://github.com/swalstab">
               <img
-                src="./src/assets/img/github-mark-white.svg"
+                src={`./src/assets/img/logo/${imgGit}`}
                 alt="logo GitHub"
-                className={styles.logo}
+                className="nav-logo"
               />
             </Link>
           </li>
           <li>
-            <NavLink to="/projects">Projects</NavLink>
-          </li>
-          <li>
-            <NavLink to="/about">About</NavLink>
+            <button className="btn-theme" onClick={handleThemeButton}>
+              <img
+                src={`./src/assets/img/logo/${imgTheme}`}
+                style={isDark || isHome ? { filter: "invert()" } : {}}
+              />
+            </button>
           </li>
         </ul>
       </nav>
