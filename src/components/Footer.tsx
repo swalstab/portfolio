758da6cf28adcf1688a81b4eb163a9bb2b378ef1
StfnVlds <stefanie.walstab@gmail.com>
2025-05-15 08:42:53 +0200

    update Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,14 @@
+import { useLocation } from "react-router-dom";
 import "./Footer.css";
 
 const currentDate: Date = new Date();
 const currentYear: number = currentDate.getFullYear();
 
 function Footer() {
+  const isHome = useLocation().pathname === "/";
+
   return (
-    <footer className="footer">
+    <footer className={`footer ${isHome ? "dark" : ""}`}>
       <div className="container">
         <p className="copyright">
           &copy; <span>{currentYear}</span> Stefanie Walstab
