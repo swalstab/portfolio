ee686814bf35cba6c99b65aa3e61808e80ba585c
StfnVlds <stefanie.walstab@gmail.com>
2025-06-02 16:42:53 +0200

    adjust translation and hover effect in AboutPage, add click events to AboutPage

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -4,55 +4,103 @@ import shoesIamge from "../assets/img/about/shoes.webp";
 import locationIcon from "../assets/img/icon/location.svg";
 
 import "./AboutPage.css";
-import HTMLReactParser from "html-react-parser/lib/index";
 import { useState } from "react";
 
 type Language = "german" | "spanish" | "english";
+interface AboutContent {
+  descriptionBeforeCoding: string;
+  descriptionCoding: string;
+  descriptionAfterCoding: string;
+  descriptionBeforeRunning: string;
+  descriptionRunning: string;
+  descriptionAfterRunning: string;
+  location: string;
+  locationRunning: string;
+}
 
-const markup: Map<Language, string> = new Map();
+type Image = "initial" | "laptop" | "shoes";
+interface VisibilityStyle {
+  opacity: "0" | "1";
+  pointerEvents: "auto" | "none";
+  visibility: "visible" | "hidden";
+}
+
+const styleVisible: VisibilityStyle = {
+  opacity: "1",
+  pointerEvents: "auto",
+  visibility: "visible",
+};
+const styleHidden: VisibilityStyle = {
+  opacity: "0",
+  pointerEvents: "none",
+  visibility: "hidden",
+};
+
+const markup: Map<Language, AboutContent> = new Map();
 markup
-  .set(
-    "english",
-    `I’m Stefanie Walstab, a frontend developer from Germany with an
-    unusual path into tech: I started in civil engineering, building
-    detailed BIM models. Along the way, I fell in love with
-    <span className="highlight coding">coding</span> and decided to make 
-    it my new career.
-    <br />
-    When I’m not coding, you’ll probably find me
-    <span className="highlight running">running</span>. I’m fascinated by 
-    the challenge of marathons and ultramarathons, and I love pushing my 
-    limits, both on the road and in code.`
-  )
-  .set(
-    "german",
-    `Ich bin Stefanie Walstab, eine Frontend-Entwicklerin aus Deutschland. 
-    Ursprünglich komme ich aus dem Bauingenieurwesen, wo ich mehrere 
-    Jahre lang komplexe BIM-Modelle erstellt habe. Dabei habe ich meine 
-    Begeisterung fürs <span className="highlight coding">Programmieren</span> 
-    entdeckt und mich entschieden, beruflich einen neuen Weg zu gehen.
-    <br />
-    Wenn ich nicht gerade code, bin ich wahrscheinlich irgendwo beim 
-    <span className="highlight running">Laufen</span>. Besonders Marathons und 
-    Ultramarathons haben es mir angetan. Es fasziniert mich, meine Grenzen 
-    auszutesten, sowohl auf der Strecke als auch im Code.`
-  )
-  .set(
-    "spanish",
-    `Soy Stefanie Walstab, una desarrolladora frontend de Alemania. Originalmente 
-    trabajé en el campo de la ingeniería civil, donde pasé varios años 
-    creando modelos BIM complejos. Durante ese tiempo descubrí mi interés 
-    por la <span className="highlight coding">programación</span> y decidí 
-    cambiar de rumbo profesional.
-    <br />
-    Cuando no estoy programando, probablemente estoy 
-    <span className="highlight running">corriendo</span>. Me atraen especialmente 
-    los maratones y ultramaratones. Me motiva poner a prueba mis límites, 
-    tanto en la pista como en el código.`
-  );
+  .set("english", {
+    descriptionBeforeCoding: `I’m Stefanie Walstab, a frontend developer
+     from Germany with an unusual path into tech: I started in civil 
+     engineering, building complex BIM models. Along the way, I fell in 
+     love with `,
+    descriptionCoding: `coding`,
+    descriptionAfterCoding: ` and decided to make it my new career.`,
+    descriptionBeforeRunning: `When I’m not coding, you’ll probably find me `,
+    descriptionRunning: `running`,
+    descriptionAfterRunning: `. I’m fascinated by the challenge of 
+      marathons and ultramarathons, and I love pushing my limits, both on 
+      the road and in code.`,
+    location: "Dresden, Germany",
+    locationRunning: `All over the world`,
+  })
+  .set("german", {
+    descriptionBeforeCoding: `Ich bin Stefanie Walstab, eine 
+      Frontend-Entwicklerin aus Deutschland. Ursprünglich komme ich aus 
+      dem Bauingenieurwesen, wo ich komplexe BIM-Modelle erstellt habe. 
+      Dabei habe ich meine Begeisterung fürs `,
+    descriptionCoding: `Programmieren`,
+    descriptionAfterCoding: ` entdeckt und beschlossen, daraus meinen 
+      neuen Beruf zu machen.`,
+    descriptionBeforeRunning: `Wenn ich nicht gerade programmiere, findet 
+      man mich wahrscheinlich beim `,
+    descriptionRunning: `Laufen`,
+    descriptionAfterRunning: `. Besonders Marathons und Ultramarathons 
+      haben es mir angetan. Es fasziniert mich, meine Grenzen 
+      auszutesten, sowohl auf der Laufstrecke als auch im Code.`,
+    location: "Dresden, Deutschland",
+    locationRunning: `Auf der ganzen Welt`,
+  })
+  .set("spanish", {
+    descriptionBeforeCoding: `Soy Stefanie Walstab, una desarrolladora 
+      frontend de Alemania con un camino poco habitual en la informática: 
+      comencé en la ingeniería civil, creando modelos BIM complejos. En el 
+      camino, descubrí mi interés por la `,
+    descriptionCoding: `programación`,
+    descriptionAfterCoding: ` y decidí convertirla en mi nueva carrera.`,
+    descriptionBeforeRunning: `Cuando no estoy programando, probablemente 
+      estoy `,
+    descriptionRunning: `corriendo`,
+    descriptionAfterRunning: `. Me fascinan especialmente los maratones 
+      y ultramaratones y me encanta superar mis límites, tanto en la 
+      carretera como en el código.`,
+    location: "Dresde, Alemania",
+    locationRunning: `En todo el mundo`,
+  });
 
 function AboutPage() {
   const [language, setLanguage] = useState<Language>("english");
+  const [displayedImg, setDisplayedImg] = useState<Image>("initial");
+  const [tempDisplayedImg, setTempDisplayedImg] = useState<Image>("initial");
+
+  const laptopImgStyle: VisibilityStyle =
+    tempDisplayedImg === "laptop" ? styleVisible : styleHidden;
+  const shoesImgStyle: VisibilityStyle =
+    tempDisplayedImg === "shoes" ? styleVisible : styleHidden;
+
+  const setInitialImg = function () {
+    setDisplayedImg("initial");
+    setTempDisplayedImg("initial");
+  };
 
   return (
     <main className="about">
@@ -68,33 +116,71 @@ function AboutPage() {
             className="about-img laptop"
             src={laptopIamge}
             alt="cartoon laptop"
+            style={laptopImgStyle}
             draggable="false"
           />
           <img
             className="about-img shoes"
             src={shoesIamge}
             alt="cartoon running shoes"
+            style={shoesImgStyle}
             draggable="false"
           />
         </div>
         <div className="about-content--text">
           <h2 className="about-heading">
-            <span className="" onMouseOver={() => setLanguage("german")}>
-              Hallo,
-            </span>
-            <span className="" onMouseOver={() => setLanguage("spanish")}>
-              Hola,
-            </span>
             <span
-              className="active-language"
-              onMouseOver={() => setLanguage("english")}
+              className={language === "german" ? "active-language" : ""}
+              onClick={() => setLanguage("german")}
+            >
+              Hallo
+            </span>
+            <span className="padding-r">,</span>
+            <span
+              className={language === "spanish" ? "active-language" : ""}
+              onClick={() => setLanguage("spanish")}
+            >
+              Hola
+            </span>
+            <span className="padding-r">,</span>
+            <span
+              className={language === "english" ? "active-language" : ""}
+              onClick={() => setLanguage("english")}
             >
               Hello
             </span>
           </h2>
 
           <p className="about-description">
-            {HTMLReactParser(markup.get(language)!)}
+            <span onClick={setInitialImg}>
+              {markup.get(language)?.descriptionBeforeCoding}
+            </span>
+            <span
+              className="highlight"
+              onClick={() => setDisplayedImg("laptop")}
+              onMouseOver={() => setTempDisplayedImg("laptop")}
+              onMouseOut={() => setTempDisplayedImg(displayedImg)}
+            >
+              {markup.get(language)?.descriptionCoding}
+            </span>
+            <span onClick={setInitialImg}>
+              {markup.get(language)?.descriptionAfterCoding}
+            </span>
+            <br />
+            <span onClick={setInitialImg}>
+              {markup.get(language)?.descriptionBeforeRunning}
+            </span>
+            <span
+              className="highlight"
+              onClick={() => setDisplayedImg("shoes")}
+              onMouseOver={() => setTempDisplayedImg("shoes")}
+              onMouseOut={() => setTempDisplayedImg(displayedImg)}
+            >
+              {markup.get(language)?.descriptionRunning}
+            </span>
+            <span onClick={setInitialImg}>
+              {markup.get(language)?.descriptionAfterRunning}
+            </span>
           </p>
 
           <div className="about-location">
@@ -104,7 +190,11 @@ function AboutPage() {
               alt="icon location"
               draggable="false"
             />
-            <p>Dresden, Germany</p>
+            <p>
+              {tempDisplayedImg === "shoes"
+                ? markup.get(language)?.locationRunning
+                : markup.get(language)?.location}
+            </p>
           </div>
         </div>
       </section>
